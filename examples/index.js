11998946b1cc1a6398bdc563a634ec34d5aa4ddc
xyq153596 <xyq153596@163.com>
2018-04-16 18:54:50 +0800

    更新readme.md

--- examples/index.js
+++ examples/index.js
@@ -14,7 +14,6 @@ const vm = new Vue({
             },
             t2: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522227246400&di=bb5ea19a3d6ebab7a83b3b393dc85018&imgtype=0&src=http%3A%2F%2Fimg5.xiazaizhijia.com%2Fwalls%2F20160708%2F1440x900_2f172c09d079701.jpg",
             custom: {
-                always: true,
                 trigger(el) {
                     console.log('执行自定义判断')
                     return true
